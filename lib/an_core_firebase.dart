0148f15e08ceaad6e370d5f1cbb073ebb79d92fc
Abdelrahmanyehia9 <136289803+Abdelrahmanyehia9@users.noreply.github.com>
2025-11-01 22:28:03 +0200

    add remote config

--- lib/an_core_firebase.dart
+++ lib/an_core_firebase.dart
@@ -7,3 +7,4 @@ export 'package:firebase_analytics/firebase_analytics.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_crashlytics/firebase_crashlytics.dart';
 export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:firebase_remote_config/firebase_remote_config.dart';
